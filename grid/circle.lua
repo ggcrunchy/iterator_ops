49ea292edac80f63d1f0ef3fce8c373a5f026f77
Steven Johnson <steve@xibalbastudios.com>
2015-03-10 01:32:47 -0600

    Circle iterator

--- grid/circle.lua
+++ grid/circle.lua
@@ -25,7 +25,9 @@
 
 -- Standard library imports --
 local abs = math.abs
+local ipairs = ipairs
 local max = math.max
+local sort = table.sort
 
 -- Modules --
 local iterator_utils = require("iterator_ops.utils")
@@ -36,52 +38,216 @@ local _CircleOctant_
 -- Exports --
 local M = {}
 
+--
+local function NegateX (coords, n)
+	for i = 1, n, 2 do
+		coords[i] = -coords[i]
+	end
+end
+
+--
+local function SwapXY (coords, n)
+	for i = 1, n, 2 do
+		coords[i], coords[i + 1] = coords[i + 1], coords[i]
+	end
+end
+
+--
+local function SwapXY_Negate (coords, n)
+	for i = 1, n, 2 do
+		coords[i], coords[i + 1] = -coords[i + 1], -coords[i]
+	end
+end
+
 -- --
-local OctantFunc = {
-	--
+local InitOctantFunc = {
+	-- (+y, +x) --
+	SwapXY,
+
+	-- (-y, +x) --
+	NegateX,
+
+	-- (-x, +y) --
+	SwapXY_Negate,
+
+	-- (-x, -y) --
+	function(coords, n)
+		for i = 2, n, 2 do
+			coords[i] = -coords[i]
+		end
+	end,
+
+	-- (-y, -x) --
+	SwapXY,
+
+	-- (+y, -x) --
+	NegateX,
+
+	-- (+x, -y) --
+	SwapXY_Negate
 }
+
+-- --
+local Coords
+
+--
+local function Quad1 (i1, i2)
+	local y1, y2 = Coords[i1], Coords[i2]
+
+	if y1 == y2 then	
+		local x1, x2 = Coords[i1 - 1], Coords[i2 - 1]
+
+		return x1 > x2
+	else
+		return y1 > y2
+	end
+end
+
+--
+local function Quad2 (i1, i2)
+	local y1, y2 = Coords[i1], Coords[i2]
+
+	if y1 == y2 then	
+		local x1, x2 = Coords[i1 - 1], Coords[i2 - 1]
+
+		return x1 > x2
+	else
+		return y1 < y2
+	end
+end
+
+--
+local function Quad3 (i1, i2)
+	local y1, y2 = Coords[i1], Coords[i2]
+
+	if y1 == y2 then	
+		local x1, x2 = Coords[i1 - 1], Coords[i2 - 1]
+
+		return x1 < x2
+	else
+		return y1 < y2
+	end
+end
+
+--
+local function Quad4 (i1, i2)
+	local y1, y2 = Coords[i1], Coords[i2]
+
+	if y1 == y2 then	
+		local x1, x2 = Coords[i1 - 1], Coords[i2 - 1]
+
+		return x1 < x2
+	else
+		return y1 > y2
+	end
+end
+
+-- --
+local SortOctantFunc = {
+	-- (+y, +x) --
+	{ pref = "x", pref_op = ">", alt_op = "<" },
+
+	-- (-y, +x) --
+	{ pref = "x", pref_op = ">", alt_op = ">" },
+
+	-- (-x, +y) --
+	{ pref = "y", pref_op = ">", alt_op = ">" },
+
+	-- (-x, -y) --
+	{ pref = "y", pref_op = ">", alt_op = "<" },
+
+	-- (-y, -x) --
+	{ pref = "x", pref_op = "<", alt_op = ">" },
+
+	-- (+y, -x) --
+	{ pref = "x", pref_op = "<", alt_op = "<" },
+
+	-- (+x, -y) --
+	{ pref = "y", pref_op = "<", alt_op = "<" }
+}
+
+--
+local function LT (a, b)
+	return a < b
+end
+
+--
+local function GT (a, b)
+	return a > b
+end
+
+--
+for i, form in ipairs(SortOctantFunc) do
+	local pdiff, pop = form.pref == "x" and 1 or 0, form.pref_op == "<" and LT or GT
+	local adiff, aop = 1 - pdiff, form.alt_op == "<" and LT or GT
+
+	SortOctantFunc[i] = function(i1, i2)
+		local pc1, pc2 = Coords[i1 - pdiff], Coords[i2 - pdiff]
+
+		if pc1 == pc2 then	
+			local ac1, ac2 = Coords[i1 - adiff], Coords[i2 - adiff]
+
+			return aop(ac1, ac2)
+		else
+			return pop(pc1, pc2)
+		end
+	end
+end
 
 --- DOCME
 M.Circle = iterator_utils.InstancedAutocacher(function()
-	local coords, oi, ofunc, i, n = {}
+	local coords, indices, oi, i, imax, n, prevx, prevy = {}, {}
 
 	-- Body --
 	return function()
-		i = i + 2
+		local x, y
 
-		return ofunc(coords, i, n)
+		repeat
+			i = i + 1
+
+			local index = indices[i]
+
+			x, y = coords[index - 1], coords[index]
+		until x ~= prevx or y ~= prevy
+
+		prevx, prevy = x, y
+
+		return x, -y
 	end,
 
 	-- Done --
 	function()
-		if i == n then
+		if i == imax then
 			if n == 0 or oi == 8 then
 				return true
 			else
-				oi, i = oi + 1, 0
-				ofunc = OctantFunc[oi]
---[[
-				if qi == 2 or qi == 3 then
-					n = n - 2
-				else
-					n = n + 2
+				InitOctantFunc[oi](coords, n)
+
+				Coords = coords
+
+				sort(indices, SortOctantFunc[oi])
+
+				oi, i, Coords = oi + 1, 0
+
+				if oi == 8 then
+					imax = imax - 1
 				end
-]]
 			end
 		end
 	end,
 
 	-- Setup --
 	function(radius)
-		oi, i, n = 0, 0, 0
+		oi, i, imax, n, prevx, prevy = 1, 0, 0, 0
 
 		for x, y in _CircleOctant_(radius) do
-			coords[n + 1], coords[n + 2], n = x, y, n + 2
+			coords[n + 1], coords[n + 2], n = x, -y, n + 2
+			indices[imax + 1], imax = n, imax + 1
 		end
 
-		coords[n + 1], coords[n + 2] = 0, -radius -- n incremented in done()
-
-		i = n -- trigger an octant switch immediately
+		for i = #indices, imax + 1, -1 do
+			indices[i] = nil
+		end
 	end
 end)
 
@@ -91,12 +257,20 @@ end)
 -- @int radius Circle radius.
 -- @treturn iterator Supplies column, row at each iteration, in order.
 M.CircleOctant = iterator_utils.InstancedAutocacher(function()
-	local x, y, diff
+	local x, y, diff, dx, dy
 
 	-- Body --
 	return function()
+		local xwas, ywas = x, y
+
 		y = y + 1
 
+		if diff >= 0 then
+			x, diff, dx = x - 1, diff - dx, dx - 2
+		end
+
+		diff, dy = diff + dy, dy + 2
+--[[
 		if y > 0 then
 			x = x - 1
 			diff = diff + y - x
@@ -107,19 +281,19 @@ M.CircleOctant = iterator_utils.InstancedAutocacher(function()
 			end
 		else
 			x = x + 1
-		end
+		end]]
 
-		return x, y
+		return xwas, -ywas
 	end,
 
 	-- Done --
 	function()
-		return x <= y
+		return x < y--x <= y
 	end,
 
 	-- Setup --
 	function(radius)
-		x, y, diff = radius - 1, -1, 0
+		x, y, diff, dx, dy = radius, 0, 1 - radius, 5 - 2 * radius, 3 --radius - 1, -1, 0
 	end
 end)
 
